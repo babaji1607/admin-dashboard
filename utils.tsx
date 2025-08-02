ce4c3ee1bf6acb6fc6d6cd9581bae6df05dfd738
babaji1607 <pishach.singh@gmail.com>
2025-08-02 19:38:54 +0530

    url changed again

--- utils.tsx
+++ utils.tsx
@@ -1,2 +1,1 @@
-// export const GLOBAL_URL = 'http://127.0.0.1:8000'
-export const GLOBAL_URL = 'http://d80o4cck8wc44kcggookwg4g.82.112.230.150.sslip.io'
+export const GLOBAL_URL = 'https://first-step-school-server.onrender.com'
